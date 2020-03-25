83c6f2d1a875d6f69f63dfa3144cf7129ab37aaf
Alec Prettyman <alecprettyman@Alecs-MBP.hsd1.ut.comcast.net>
2020-03-25 15:14:37 -0600

    app-2 completed

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      data: ['MW', 'Smash Bros', 'Legend of Zelda', 'Shadow of Mordor']
+    }
+  }
+
+  diplaystuff(){
+    let shown = this.state.data.map((e, i) => {
+    return <h2 key={i}>{e}</h2>
+    })
+    return shown
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.diplaystuff()}
+      </div>
+    );
+  }
 }
 
 export default App;
